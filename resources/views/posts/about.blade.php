046e30edb3454d1c688fdbe0af7fd2665f129011
futoshi-endo <futoshi.endo@pepabo.com>
2016-11-15 11:59:30 +0900

    細かく修正しました

--- resources/views/posts/about.blade.php
+++ resources/views/posts/about.blade.php
@@ -9,14 +9,11 @@
 <!-- <h1>Welcome EndoBlog!</h1> -->
 @endsection
 
-@section('img1')
-<img src="/assets/img/top.jpg" alt="" width="600" height="400">
-@endsection  
 
 @section('about')
 <h2  class="message" style="background:skyblue;">DuPoの使い方</h2>
 <p>DuPoは今日一日あった良い事やくだらないことをとりあえず書いてみる<u>日報サービス</u>です!
-因みに現在急ピッチで製作しています<strong>遠藤</strong>だけです。</p>
+因みに現在急ピッチで製作しています。</p>
 
 @endsection
 
