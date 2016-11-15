7d047a403b71ed4a249d3f2edfe390bdc1aaa0d4
futoshi-endo <futoshi.endo@pepabo.com>
2016-11-15 11:50:36 +0900

    一旦aboutページを修正

--- resources/views/posts/aboutme.blade.php
+++ resources/views/posts/aboutme.blade.php
@@ -9,18 +9,12 @@ About me
 <!-- <h1>Welcome EndoBlog!</h1> -->
 @endsection
 
-@section('img1')
-<img src="/assets/img/me.jpg" alt="fendo181" width="280" height="280">
-@endsection  
 
 @section('aboutme')
-<h2>製作者</h2>
-<p>名前:<a href="https://github.com/Fendo181">Fendo181</a></p>
+<h2>製作者:えんどぅー</h2>
 <p>年齢:22歳<p>
-<p>大学選考:情報通信工学</p>
-<p>趣味:webサービス開発</p>
-<p>一言:このブログサービスもっとよくしたいですね。手抜きですみません。</p>
-<p>もう一言:自作でPHPのフレームワーク作りたい!</p>
+<p>一言:手抜きですみません。</p>
+
 
 @endsection
 
