3ff39dba56c4dd34a7bd1d85f029622503de9be9
F_endou181 <fendo181git@gmail.com>
2016-12-14 03:58:50 +0900

    "DupoController を綺麗に修正しました"

--- app/Http/Controllers/DupoController.php
+++ app/Http/Controllers/DupoController.php
@@ -13,13 +13,7 @@ class DupoController extends Controller
 
         public function top(){
             $nipos = Nipo::latest('created_at')->get();
-
-            // 日本形式の時刻に合わせる。
-            // $date = $nipos->created_at;
-            // $time_japan = date('Y年n月j日', strtotime($date));
-            // // withでデータをviewへ渡す。
             return view('dupos.dupo')->with('nipos',$nipos);
-                                    //  ->with('time_japan',$time_japan);
 
         }
 
@@ -27,30 +21,21 @@ class DupoController extends Controller
             return redirect('/dupo')->with('flash_message', 'root権限を手に入れました!');
         }
 
-        // 引数に$idを受け取る。
-        public function show($id){
-            // 見つかったらとりあえず、とりあえず使っまえて来る。
-            // $nipos = Nipo::find('id');
-
-            // 見つからない場合は例外を投げる。
-            $nipo = Nipo::findOrFail($id);
-            return view('dupos.show')->with('nipo',$nipo);
-        }
-
-        // 単純に記事を投稿するページへ飛ばす。
         public function create(){
            return view('dupos.create');
         }
 
+        public function show($id){
+            $nipo = Nipo::findOrFail($id);
+            return view('dupos.show')->with('nipo',$nipo);
+        }
 
         public function edit($id){
-            //idが無かったら例外処理を行う
             $nipo = Nipo::findOrFail($id);
             return view('dupos.edit')->with('nipo',$nipo);
         }
 
 
-        //storeメソッド
         public function store(NipoRequest $request){
             $nipo = new Nipo();
             $nipo->title =$request->title;
@@ -68,23 +53,8 @@ class DupoController extends Controller
             return redirect('/dupo')->with('flash_message', '記事を更新しました。');
         }
         public function destroy($id){
-           //idが無かったら例外処理を行う
            $nipo = Nipo::findOrFail($id);
            $nipo -> delete();
            return redirect('/dupo')->with('flash_message', '記事を消去しました。');
        }
-
-
-
-
-
-        //debag用
-        // public function dd(){
-        //
-        //     $nipos = Nipo::latest('created_at')->get();
-        //     dd($nipos->toArray());
-        //     return view('nipos.dd');
-        // }
-
-
 }
