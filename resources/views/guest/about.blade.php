4220b2712015209200a4aae5c7148fa42ef95083
F_endou181 <fendo181git@gmail.com>
2016-12-14 03:22:55 +0900

    "新しいabout,aboutmeページを作成"

---
+++ resources/views/guest/about.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.default')
+
+@section('title')
+DuPoとは?
+@endsection
+
+
+@section('blog_title')
+<!-- <h1>Welcome EndoBlog!</h1> -->
+@endsection
+
+
+@section('about')
+<h2  class="message" style="background:skyblue;">DuPoとは?</h2>
+<p>DuPoは遠藤が<u>勢いで作ってしまった日報サービス</u>です。</p>
+<p>日報的な事はできますが、それ以上を望むと制作者である遠藤が夜逃げするので、止めて下さい。</p>
+
+
+@endsection
+
+@section('tab_menus_down')
+<ul class="nav nav-tabs">
+    <li class="nav-item">
+        <a class="nav-link" href="{{ url('/guest_dupo') }}"><i class="fa fa-fw fa-book" aria-hidden="true"></i>Back DuPo Page</a>
+    </li>
+     <li class="nav-item">
+         <a class="nav-link" href="{{ url('/guest_aboutme') }}"><i class="fa fa-fw fa-male" aria-hidden="true"></i>About me</a>
+     </li>
+     <li class="nav-item">
+         <a class="nav-link" href="https://github.com/Fendo181/DuPo"><i class="fa fa-fw fa-github" aria-hidden="true"></i>GitHub</a>
+     </li>
+</ul>
+@endsection
---
+++ resources/views/guest/aboutme.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.default')
+
+@section('title')
+About me
+@endsection
+
+
+@section('blog_title')
+<!-- <h1>Welcome EndoBlog!</h1> -->
+@endsection
+
+
+@section('aboutme')
+<div class="container">
+<h2  class="message" style="background:skyblue;">製作者:えんどぅー</h2>
+<p>年齢:23歳<p>
+<p>しいたけが食べれないです。</p>
+</div>
+@endsection
+
+@section('tab_menus_down')
+<ul class="nav nav-tabs">
+    <li class="nav-item">
+         <a class="nav-link" href="{{ url('/guest_dupo') }}"><i class="fa fa-fw fa-book" aria-hidden="true"></i>Back DuPo Page</a>
+     </li>
+     <li class="nav-item">
+         <a class="nav-link" href="{{ url('/guest_about') }}"><i class="fa fa-fw fa-question" aria-hidden="true"></i>DuPoとは</a>
+     </li>
+     <li class="nav-item">
+         <a class="nav-link" href="https://github.com/Fendo181/DuPo"><i class="fa fa-fw fa-github" aria-hidden="true"></i>GitHub</a>
+     </li>
+</ul>
+@endsection
